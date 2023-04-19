42b35127fd3b56c6411c70ec3f3b9bef76bccc44
darryn.campbell <darryn.campbell@pubnub.com>
2023-04-19 14:49:28 +0100

    Adding Twitter stream logic back into Demo

--- js/streams.js
+++ js/streams.js
@@ -7,15 +7,15 @@ var streams = {
       subKey: 'N/A',
       channel: 'N/A'
     },
-    /*{
+    {
       id: 'twitter',
-      name: 'Twitter Stream (Live)',
+      name: 'Twitter Stream',
       description:
         'A real-time stream of actual messages on Twitter.  For readability, capped at 10 tweets per second.',
       subKey: 'sub-c-d00e0d32-66ac-4628-aa65-a42a1f0c493b',
       //  OLD: "subKey": "sub-c-78806dd4-42a6-11e4-aed8-02ee2ddab7fe",
       channel: 'pubnub-twitter'
-    },*/
+    },
     {
       id: 'wikipedia',
       name: 'Wikipedia Changes (Live)',
